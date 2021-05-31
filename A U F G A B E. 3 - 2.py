838fbb10334caccc00ef26ac4a8312efa077ab4d
greta-gerasimov <kaygerasimova@gmail.com>
2021-05-31 09:22:41 +0300

    add homework 3.2

---
+++ A U F G A B E. 3 - 2.py
@@ -0,0 +1,12 @@
+#что делать с выводом?
+
+#2.
+# Реализовать функцию, принимающую несколько параметров,
+# описывающих данные пользователя: имя, фамилия, год рождения,
+# город проживания, email, телефон.
+# Функция должна принимать параметры как именованные аргументы.
+# Реализовать вывод данных о пользователе одной строкой.
+def create_candy_cane_shotgun(name, surname, age, city, email, cellnumber):
+    print(name, surname, age, city, email, cellnumber)
+
+create_candy_cane_shotgun(name = 'Lumpy Space Princess', surname = 'Lumpy ', age = '15', city = 'Lumpy Space', email = 'Gotcha@gmail.com', cellnumber = '123758493')
